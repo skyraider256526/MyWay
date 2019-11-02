f723cccfca1421c9f6bf9e6805db983f39d461eb
skyraider <akashyadav@gmail.com>
2019-11-02 19:53:09 +0530

    Updates

---
+++ precache-manifest.e48ed610d855c901643d755ab935afbe.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "203c4e52f1390d74a40287a0a9894c54",
+    "url": "/MyWay/index.html"
+  },
+  {
+    "revision": "c71c922b81e8da095c76",
+    "url": "/MyWay/static/css/main.1969915b.chunk.css"
+  },
+  {
+    "revision": "5b36ef7f6ed6cc8515eb",
+    "url": "/MyWay/static/js/2.c9a16ca0.chunk.js"
+  },
+  {
+    "revision": "c71c922b81e8da095c76",
+    "url": "/MyWay/static/js/main.c7836a28.chunk.js"
+  },
+  {
+    "revision": "da892160f31dc977b297",
+    "url": "/MyWay/static/js/runtime-main.8b4a8719.js"
+  }
+]);
