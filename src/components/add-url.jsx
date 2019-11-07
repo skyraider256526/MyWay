4c49ad77f8132b77fc1665525d6f578982f4cbe8
skyraider <akashyadav@gmail.com>
2019-11-07 09:32:48 +0530

    added onKeyDown event

--- src/components/add-url.jsx
+++ src/components/add-url.jsx
@@ -5,6 +5,7 @@ import { connect } from "react-redux";
 import { addUrlToPlaylist } from "../redux/playlist.action";
 
 export const AddUrl = ({ dispatch }) => {
+  const ENTER_KRY = 13;
   //urlCheck1 for url copied from the addres bar, with http:// and without
   const urlCheck1 = url =>
     /http:\/\/www.youtube.com\/watch\?v=.\w/.test(url) ||
@@ -12,6 +13,8 @@ export const AddUrl = ({ dispatch }) => {
   //urlCheck2 for url gotten by using sharing option in a video, with http:// and without
   const urlCheck2 = url =>
     /http:\/\/youtu\..\D\/\w/.test(url) || /youtu\..\D\/\w/.test(url);
+  //combinig the above two checks
+  const urlCheck = value => urlCheck1(value) || urlCheck2(value);
   return (
     <input
       className="border border-success"
@@ -19,13 +22,25 @@ export const AddUrl = ({ dispatch }) => {
       placeholder="enter url here"
       onBlur={event => {
         const value = event.target.value;
-        event.target.value = null;
-        console.log("url Check :", urlCheck1(value) || urlCheck2(value));
-        if (urlCheck1(value) || urlCheck2(value)) {
-          return dispatch(addUrlToPlaylist(value));
-        }
-        if (value !== "") {
+        console.log("url Check :", urlCheck(value));
+        if (urlCheck(value)) {
+          dispatch(addUrlToPlaylist(value));
+          event.target.value = null;
+          return; //return used to skip the below window.alert()
+        } else if (value !== "") {
           window.alert("enter a valid youtube url");
+        }
+      }}
+      onKeyDown={e => {
+        if (e.keyCode === ENTER_KRY) {
+          const value = e.target.value;
+          if (urlCheck(value)) {
+            dispatch(addUrlToPlaylist(value));
+            e.target.value = null;
+          } else if (value !== "") {
+            window.alert("enter valid youtube url");
+            e.target.value = null;
+          }
         }
       }}
     />
